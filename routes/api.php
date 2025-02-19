1fb47866c5efbf7e0123ca73ea6d5ca821decc24
DSK-DEV-02049 <joao.santos@fabricainfo.com>
2025-02-19 16:31:43 -0300

    [feat] auth
    inicio autenticação

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+
+Route::middleware(['guest'])->group(function () {
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->name('register');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->name('login');
+
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
+
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+        ->name('password.store');
+});
